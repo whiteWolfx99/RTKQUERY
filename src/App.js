4c2c4ef651d198580b96f45c1bd31c747a46ba0d
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-11-20 23:38:59 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useGetTodosQuery,
+  usePostTodoMutation,
+  useDeleteTodoMutation,
+  useUpdateTodoMutation,
+} from "./api/apiSlice";
+import { useState } from "react";
+import React from "react";
+import "./App.css";
 
 function App() {
+  const { data } = useGetTodosQuery();
+  const [todo, setTodo] = useState("");
+  // isedit
+   const [newedit, setnewedit] = useState("");
+  // edit
+  const [edit, setedit] = useState([]);
+
+  const [addTodo] = usePostTodoMutation();
+  const [DeleteTodo] = useDeleteTodoMutation();
+  const [UpdateTodo] = useUpdateTodoMutation();
+
+  const handlesubmit = (e) => {
+    if (todo !== "") {
+      e.preventDefault();
+      addTodo({ userId: 1, title: todo, completed: false });
+      setTodo("");
+    }
+  };
+
+  const handlesave = (e) => {
+    if (newedit !== "") {
+      e.preventDefault();
+      UpdateTodo({ userId: 1, id: edit.id, title: newedit, completed: false });
+      setnewedit("");
+    }
+  };
+
   return (
     <div className="App">
+      <div className="addform">
+        {/* add */}
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="add..."
+        ></input>
+        <button onClick={handlesubmit}>Add</button>
+        {/* update */}
+        <input
+          type="text"
+          value={newedit ? newedit : edit.title}
+          onChange={(e) => setnewedit(e.target.value)}
+          placeholder="update..."
+        ></input>
+        <button onClick={handlesave}>save</button>
+      </div>
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data?.map((todo) => (
+          <div key={todo.id}>
+            <div className={todo.completed ? "box2" : "box"}>
+              <div className="title">
+                <h3>{todo.id}</h3>
+                <h3>{todo.title}</h3>
+              </div>
+              <div className="btn">
+                <button
+                  onClick={() => DeleteTodo({ id: todo.id })}
+                  className="delete"
+                >
+                  Delete
+                </button>
+                <button onClick={() => setedit(todo)} className="Edit">
+                  Edit
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </header>
     </div>
   );
